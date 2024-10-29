529a1fe6a6f332239afdb1c20da9610c7d37786c
Shivani Choudhary <anujdbgt@gmail.com>
2024-10-29 13:57:38 +0530

    29th Oct Fish

---
+++ run.bat
@@ -0,0 +1,2 @@
+"C:\Program Files\Java\jre1.8.0_421\bin\java.exe" -server -Xmx6G -Xms6G -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=50 -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20  -XX:G1HeapRegionSize=32M -jar server.jar nogui
+pause
